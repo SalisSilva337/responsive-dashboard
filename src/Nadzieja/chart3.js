4671f15a18eab3b0d93b9eb985b7aea2e72f732a
Salis Silva <salis.silva@aluno.senai.br>
2024-09-03 02:41:24 -0300

    mais um grafico adicionado

---
+++ src/Nadzieja/chart3.js
@@ -0,0 +1,30 @@
+let ctx3 = document.getElementById("barChart3");
+
+let barChart3 = new Chart(ctx3, {
+  type: "bar",
+  data: {
+    labels: [
+      "Segunda",
+      "Terça",
+      "Quarta",
+      "Quinta",
+      "Sexta",
+      "Sábado",
+      "Domingo",
+    ],
+    datasets: [
+      {
+        label: "Nº de sessões",
+        data: [12, 45, 3, 5, 2, 3, 10],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
